629d685a70fbf939726f75c66389126122639c7a
msd8181 <168234769+msd8181@users.noreply.github.com>
2025-10-21 17:04:58 +0330

    Add SQLite database connection to Flask app

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, g
+
+DATABASE = 'db/database.db'
+db_name  = DATABASE.rsplit('/')[-1]
+
 app = Flask(__name__)
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    cur = get_db().cursor()
+    if cur:
+        return f'connected to {db_name} successfully!'
+    return 'not connected!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
